28e7599c9f88dcbba768e9519b3e7a75bf7c3a9e
Mike Kelly <mike@kellyfam.net>
2022-05-19 22:35:34 -0400

    updated resume for May 2022

--- src/resume.js
+++ src/resume.js
@@ -20,10 +20,11 @@ export default {
       startYear: "2019",
       endYear: null,
       highlights: [
-        "Front End Team Lead for a flagship web application delivering timely information for worldwide customers using Angular 8. Responsible for mentoring junior developers on the team in best practices. (2020-present)",
+        "Developer Lead for a web application providing search capabilities for a large datastore using Angular 11. Responsible for mentoring junior team members and ensuring best practices are followed. (2021-present)",
+        "Front End Team Lead for a flagship web application delivering timely information for worldwide customers using Angular 8. Responsible for mentoring junior developers on the team in best practices. (2020-2021)",
         "UI Team Lead responsible for modernizing legacy Angular/Node password management application using React. Architected repeatable deploy process using Docker and AWS. (2019-2020)",
       ],
-      skills: ["HTML", "CSS", "React JS", "Angular 8", "NodeJS", "Git", "Docker", "AWS"],
+      skills: ["HTML", "CSS", "React JS", "Angular 8,11", "NodeJS", "Git", "Docker", "AWS"],
     },
     {
       title: "Expert Software Engineer",
